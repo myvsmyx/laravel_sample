257af93242b0bbdeda74e2110862d39ca5b16abd
vagrant <vagrant@homestead>
2018-09-24 15:44:32 +0000

    添加注册提示

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,4 +22,26 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    /**
+     * 注册 表单验证
+     */
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        //注册成功提示
+        session()->flash('success', '欢迎，牛气哄哄的Laravel');
+        return redirect()->route('users.show', [$user]);
+    }
 }
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -8,6 +8,7 @@
     @include('layout._header')
 
     <div class="container">
+      @include('shared._messages')
       @yield('content')
       @include('layout._footer')
     </div>    
