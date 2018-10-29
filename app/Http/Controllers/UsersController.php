0e4bb1d7ed8d93daf1a4bc2fb69b9bdd6c87e30f
vagrant <vagrant@homestead>
2018-10-29 23:54:04 +0000

    邮件发送

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -75,13 +75,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'mike@qq.com';
-        $name = 'MikeMei';
         $to = $user->email;
         $subject = "感谢注册 MikeMei的 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
